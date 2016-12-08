3b9c5d236265e38c64be5a8c9a05540ebf8485d5
Alexander Zvyagintsev <alzv0411@gmail.com>
2016-12-08 18:11:55 +0400

    add servlets skeleton

---
+++ src/main/java/ru/ncedu/ecomm/servlets/ProfileServlet.java
@@ -0,0 +1,24 @@
+package ru.ncedu.ecomm.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ProfileServlet", urlPatterns = {"/profile"})
+public class ProfileServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/views/pages/profile.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // handle operations
+        req.getRequestDispatcher("/views/pages/profile.jsp").forward(req, resp);
+    }
+
+}
--- src/main/webapp/views/pages/index.jsp
+++ src/main/webapp/views/pages/index.jsp
@@ -22,6 +22,17 @@
 
     <c:import url="../fragments/topPanel.jsp"/>
 
+    <h1>Pages:</h1>
+    <a href="<c:url value="/cart"/>">cart</a><br>
+    <a href="<c:url value="/category"/>">category</a><br>
+    <a href="<c:url value="/login"/>">login</a><br>
+    <a href="<c:url value="/orders"/>">ordersHistory</a><br>
+    <a href="<c:url value="/recovery"/>">passwordRecovery</a><br>
+    <a href="<c:url value="/product"/>">product</a><br>
+    <a href="<c:url value="/profile"/>">profile</a><br>
+    <a href="<c:url value="/registration"/>">registration</a><br>
+    <a href="<c:url value="/users"/>">users</a><br>
+
     <c:import url="../components/etalon.jsp">
         <c:param name="showCheckbox" value="true"/>
     </c:import>
