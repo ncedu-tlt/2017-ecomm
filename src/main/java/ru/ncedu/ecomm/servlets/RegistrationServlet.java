75d9d329b8d5ba91718d22d9b4633f000ff36191
Anton Kuzmin <Garthdex@gmail.com>
2016-12-24 05:13:00 +0400

    Чутка начал делать

--- src/main/java/ru/ncedu/ecomm/servlets/RegistrationServlet.java
+++ src/main/java/ru/ncedu/ecomm/servlets/RegistrationServlet.java
@@ -1,11 +1,16 @@
 package ru.ncedu.ecomm.servlets;
 
+import static ru.ncedu.ecomm.data.DAOFactory.getDAOFactory;
+import ru.ncedu.ecomm.data.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "RegistrationServlet", urlPatterns = {"/registration"})
 public class RegistrationServlet extends HttpServlet {
@@ -18,7 +23,28 @@ public class RegistrationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // handle operations
-        req.getRequestDispatcher("/views/pages/registration.jsp").forward(req, resp);
+
+        if(!req.getParameter("email").isEmpty()
+                && !req.getParameter("password").isEmpty()
+                && !req.getParameter("ConfirmPassword").isEmpty()) {
+            if (checkEmail(req.getParameter("email"))) {
+                if (req.getParameter("password").equals(req.getParameter("ConfirmPassword"))) {
+                    User user = new User();
+                    user.setEmail(req.getParameter("email"));
+                    user.setPassword(req.getParameter("password"));
+                    getDAOFactory().getUserDAO().addUser(user);
+                } else resp.getWriter().write("пароли не совпали");
+            } else  resp.getWriter().write("wrong email, чисто для теста");
+        } else  resp.getWriter().write("Строки не должны быть пустыми, тоже для теста");
+
+        //req.getRequestDispatcher("/views/pages/registration.jsp").forward(req, resp);
+    }
+
+    public static boolean checkEmail(String email){
+        Pattern patternEmailValidation = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = patternEmailValidation.matcher(email);
+
+        return matcher.find();
     }
 
 }
--- src/main/webapp/views/components/registration.jsp
+++ src/main/webapp/views/components/registration.jsp
@@ -6,7 +6,7 @@
             Registration
         </h2>
     </div>
-    <form class="ui equal dividing width form">
+    <form action= "/registration" method="post" class="ui equal dividing width form">
         <div class="field">
             <label for="email">Your email:</label>
             <input type="text" name="email" id="email" placeholder="E-mail">
@@ -15,11 +15,16 @@
             <label for="password">Your password</label>
             <input type="password" name="password" id="password" placeholder="Password">
         </div>
-
-        <div class="ui blue animated fade button " tabindex="0">
-            <div class="visible content">Registration</div>
-            <div class="hidden content">Send data?</div>
+        <div class="field">
+            <label for="ConfirmPassword">Confirm password</label>
+            <input type="password" name="ConfirmPassword" id="ConfirmPassword" placeholder="Confirm password">
         </div>
+
+        <input class="ui blue animated fade button" type="submit" value="Registration" />
+        <%--<div class="ui blue animated fade button " tabindex="0">--%>
+            <%--<div class="visible content">Registration</div>--%>
+            <%--<div class="hidden content">Send data?</div>--%>
+        <%--</div>--%>
 
         <p>Already a member? <a href='<c:url value="/login"/>'> Login </a></p>
     </form>
