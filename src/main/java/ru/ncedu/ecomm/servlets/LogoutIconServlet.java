ad8645861226039287e888576db41d1265dec638
AndrewKozyrin <kozyrin.ai@gmail.com>
2017-01-19 12:50:42 +0400

    Added LogoutIconServlet, added logout button. If user not login, button logout hidden.

---
+++ src/main/java/ru/ncedu/ecomm/servlets/LogoutIconServlet.java
@@ -0,0 +1,28 @@
+package ru.ncedu.ecomm.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutIconServlet", urlPatterns = {"/logoutIcon"})
+public class LogoutIconServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logoutIcon(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    private void logoutIcon(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("userId");
+        req.getRequestDispatcher("/home").forward(req, resp);
+    }
+}
--- src/main/webapp/views/components/profileIcon.jsp
+++ src/main/webapp/views/components/profileIcon.jsp
@@ -1,7 +1,15 @@
-<form action="/profileIcon">
-    <div class="ui item">
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<div class="ui item">
+    <form action="/profileIcon">
         <button class="circular ui icon button" type="submit">
             <i class="icon user"></i>
         </button>
-    </div>
-</form>
+    </form>
+    <form action="/logoutIcon">
+    <c:if test="${sessionScope.userId != null}">
+        <button class="circular ui icon button" type="submit">
+            <i class="icon sign out"></i>
+        </button>
+    </c:if>
+    </form>
+</div>
