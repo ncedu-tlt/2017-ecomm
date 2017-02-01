e1ddcb6fdfea16f33f69393b48773a511e18adc2
AndrewKozyrin <kozyrin.ai@gmail.com>
2017-02-01 17:50:16 +0400

    Added check on users for cartServlet and order history

--- src/main/java/ru/ncedu/ecomm/servlets/CartServlet.java
+++ src/main/java/ru/ncedu/ecomm/servlets/CartServlet.java
@@ -1,5 +1,7 @@
 package ru.ncedu.ecomm.servlets;
 
+import ru.ncedu.ecomm.servlets.services.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,12 +14,13 @@ public class CartServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserService.getInstance().redirectToLoginIfNeeded(req, resp);
         req.getRequestDispatcher("/views/pages/cart.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // handle operations
+        UserService.getInstance().redirectToLoginIfNeeded(req, resp);
         req.getRequestDispatcher("/views/pages/cart.jsp").forward(req, resp);
     }
 
