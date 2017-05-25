60154c3fad188a8fae91ae2bdef6660f3614d925
jegius <jegius@gmail.com>
2017-05-26 00:23:30 +0300

    исправлен баг с неостанавливающейся очередью событий в review

--- src/main/webapp/src/app/components/review/reviews.js
+++ src/main/webapp/src/app/components/review/reviews.js
@@ -1,41 +1,89 @@
 (function ($, window) {
 
     var frm = window.frm;
+
+    var ELEMENTS = {
+        RATING: '.jsUsersRating',
+        REVIEW_RATING: '.jsUserReviewRating',
+        REMOVE_BUTTON: '.jsRemoveButton',
+        REMOVE_FORM: '.jsRemoveForm',
+        EDIT_BUTTON: '.jsEdit',
+        NEGATIVE_BUTTON: '.jsNegative',
+        PARENT_REVIEW: '.jsThisUserReview .jsReviewParent',
+        CHANGE_RATING: '.jsEditRating',
+        REVIEW_TEXT: '.jsReview',
+        REVIEW_DATA: '.jsReviewData',
+        EDIT_FIELD: '.jsReviewEditData',
+        REVIEW_BODY: '.jsThisUserReview',
+        CANCEL_BUTTON: '.jsCancel',
+        EDIT_RATING: '.jsRating'
+
+    };
+
+    var EVENTS = {
+        DISABLE: 'disable',
+        ON_RATE: 'onRate',
+        LOADING: 'loading',
+        CLICK: 'click'
+    };
+
+    var ACTIONS = {
+        EDIT: 'edit',
+        HIDE: 'hide'
+    };
+
+    var PROPERTY = {
+        SETTINGS: 'setting',
+        RATING: 'rating',
+        VALUE: 'value',
+        FAST: 'fast'
+    };
+
+    var LINKS = {
+        REVIEW: '/review'
+    };
+
     var ProductComponent = frm.inheritance.inherits(frm.components.Component, {
 
         /**
          * Executed on component initialization
          */
         init: function () {
-            this.content.find('.jsUsersRating')
+            this.content.find(ELEMENTS.RATING)
                 .rating({initialRating: 2, maxRating: 5})
-                .rating('disable');
+                .rating(EVENTS.DISABLE);
 
-            this.content.find('.jsUserReviewRating')
+            this.content.find(ELEMENTS.REVIEW_RATING)
                 .rating({initialRating: 2, maxRating: 5})
-                .rating('setting', 'onRate', this.onRatingChange.bind(this));
+                .rating(PROPERTY.SETTINGS, EVENTS.ON_RATE, this.onRatingChange.bind(this));
 
-            this.removePopup = this.content.find('.jsRemoveButton')
+            this.removePopup = this.content.find(ELEMENTS.REMOVE_BUTTON)
                 .popup({
-                    popup: this.content.find('.jsRemoveForm'),
-                    on: 'click'
+                    popup: this.content.find(ELEMENTS.REMOVE_FORM),
+                    on: EVENTS.CLICK
                 });
 
-            this.content.find('.jsThisUserReview .jsEdit').on('click', this.getData.bind(this));
+            this.content.find(ELEMENTS.EDIT_BUTTON).on(EVENTS.CLICK, function (event) {
+                var $this = $(event.currentTarget);
+                $this.addClass(EVENTS.LOADING).unbind(EVENTS.CLICK);
+                this.getData(event).bind(this)
 
-            this.content.find('.jsNegative').on('click', this.hidePopup.bind(this));
+            }.bind(this));
+
+            this.content.find(ELEMENTS.NEGATIVE_BUTTON).on(EVENTS.CLICK, this.hidePopup.bind(this));
 
         },
-        getData: function (e) {
+        getData: function (event) {
+            var $this = $(event.currentTarget);
+            var reviewBody = $this.closest(ELEMENTS.REVIEW_BODY);
+            var thisUserRating = reviewBody.find(ELEMENTS.RATING).data(PROPERTY.RATING);
+            var reviewText = reviewBody.find(ELEMENTS.REVIEW_TEXT).text();
+            var productId = reviewBody.data(PROPERTY.VALUE);
 
-            var reviewBody = this.content.find('.jsThisUserReview');
-            var thisUserRating = reviewBody.find('.jsUsersRating').data('rating');
-            var reviewText = reviewBody.find('.jsReview').text();
-            var productId = this.content.find('.jsThisUserReview').data('value');
 
-            $.post(this.params.baseUrl + '/review',
+            $.post(this.params.baseUrl + LINKS.REVIEW,
                 {
-                    reviewActions: 'edit',
+                    reviewActions: ACTIONS.EDIT,
                     productId: productId,
                     review: reviewText,
                     rating: thisUserRating
@@ -44,42 +92,50 @@
         },
 
         writeFormForUpdateComment: function (data) {
-            var reviewParent = this.content.find('.jsThisUserReview .jsReviewParent');
-            var reviewData = reviewParent.find('.jsReviewData');
+            var reviewParent = this.content.find(ELEMENTS.PARENT_REVIEW);
+            var reviewData = reviewParent.find(ELEMENTS.REVIEW_DATA);
 
 
-
-            reviewData.fadeOut('fast', function () {
+            reviewData.fadeOut(PROPERTY.FAST, function () {
                 reviewParent.append(data);
 
 
-                var editData = reviewParent.find('.jsReviewEditData');
-                var ratingField = editData.find('.jsEditRating');
-                var cancelButton = editData.find('.jsCancel');
+                var editData = reviewParent.find(ELEMENTS.EDIT_FIELD);
+                var ratingField = editData.find(ELEMENTS.CHANGE_RATING);
+                var cancelButton = editData.find(ELEMENTS.CANCEL_BUTTON);
 
-                ratingField.rating('setting', 'onRate', function (value) {
+                ratingField.rating(PROPERTY.SETTINGS, EVENTS.ON_RATE, function (value) {
                     ratingField.val(value);
                 });
 
-                cancelButton.on('click', this.cancelEdit.bind(this));
+                cancelButton.on(EVENTS.CLICK, this.cancelEdit.bind(this));
 
             }.bind(this));
         },
         cancelEdit: function () {
-            var reviewParent = this.content.find('.jsReviewParent');
-            var reviewData = reviewParent.find('.jsReviewData');
-            var editData = reviewParent.find('.jsReviewEditData');
+            var reviewParent = this.content.find(ELEMENTS.PARENT_REVIEW);
+            var reviewData = reviewParent.find(ELEMENTS.REVIEW_DATA);
+            var editData = reviewParent.find(ELEMENTS.EDIT_FIELD);
+            var editButton = reviewData.find(ELEMENTS.EDIT_BUTTON);
+
+            editButton.removeClass(EVENTS.LOADING).on(EVENTS.CLICK, function (event) {
+
+                var $this = $(event.currentTarget);
+                $this.addClass(EVENTS.LOADING).unbind(EVENTS.CLICK);
+                this.getData(event).bind(this)
+
+            }.bind(this));
 
             reviewData.fadeIn();
             editData.remove();
 
         },
         onRatingChange: function (value) {
-            this.content.find('.jsRating').val(value);
+            this.content.find(ELEMENTS.EDIT_RATING).val(value);
 
         },
         hidePopup: function () {
-            this.removePopup.popup('hide');
+            this.removePopup.popup(ACTIONS.HIDE);
         }
     });
 
